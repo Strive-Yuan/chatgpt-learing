4603294f32051d7bcbd3547bb623b9c3cffe5501
Strive.Yuan <71480812+Strive-Yuan@users.noreply.github.com>
2023-04-28 10:50:45 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>learning</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          仅个人学习使用哦~ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; -Strive
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
